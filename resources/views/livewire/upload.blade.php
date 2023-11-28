b71bb4c45cb721e7e18b98da7fabde7bc88906fa
curder <q.curder@gmail.com>
2023-11-28 17:13:48 +0800

    Showing progress and handling success

--- resources/views/livewire/upload.blade.php
+++ resources/views/livewire/upload.blade.php
@@ -1,25 +1,44 @@
-<form x-data="{
-    uploader: null,
+<form class="space-y-4"
+      x-data="{
+        uploader: null,
+        progress: 0,
 
-    submit() {
-        const file = $refs.file.files[0]
-        if (!file) {
-            return
-        }
+        submit() {
+            const file = $refs.file.files[0]
+            if (!file) {
+                return
+            }
 
-        this.uploader = createUpload({
-            file,
-            endpoint: '{{ route('files.store') }}',
-            method: 'post',
-            headers: {
-              'X-CSRF-TOKEN': '{{ csrf_token() }}',
-            },
-            chunkSize: 10 * 1024, // 10mb
-        })
-    }
+            this.uploader = createUpload({
+                file,
+                endpoint: '{{ route('files.store') }}',
+                method: 'post',
+                headers: {
+                  'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                },
+                chunkSize: 10 * 1024, // 10mb
+            })
+
+            this.uploader.on('progress', (progress) => {
+                this.progress = progress.detail
+            })
+
+            this.uploader.on('success', () => {
+                this.uploader = null
+                this.progress = 0
+            })
+        },
 }" @submit.prevent="submit">
     <div class="flex items-center">
         <input type="file" name="file" x-ref="file" class="flex-grow"/>
         <x-primary-button>Upload</x-primary-button>
     </div>
+
+    <template x-if="uploader">
+        <div>
+            <div class="w-full rounded-full bg-gray-100 overflow-hidden">
+                <div class="bg-blue-500 h-4 transform-all duration-200" :style="{width: `${progress}%`}"></div>
+            </div>
+        </div>
+    </template>
 </form>
