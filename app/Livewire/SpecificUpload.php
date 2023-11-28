b9e712e925386e84c70a6a99f37c2d1deddc54a6
curder <q.curder@gmail.com>
2023-11-28 18:32:29 +0800

    Storing the file

--- app/Livewire/SpecificUpload.php
+++ app/Livewire/SpecificUpload.php
@@ -3,12 +3,15 @@
 namespace App\Livewire;
 
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
 
 class SpecificUpload extends Upload
 {
-
     public function onSuccess(UploadedFile $file)
     {
-        dd($file);
+        auth()->user()->files()->create([
+            'name' => $file->getClientOriginalName(),
+            'path' => $file->storeAs('files', Str::uuid() . '.' . $file->getClientOriginalExtension(), 'public')
+        ]);
     }
 }
